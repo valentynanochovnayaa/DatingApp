29724d6a203c59df0caf6f7d06dfac3a929b55f8
unknown <mimi.cry4848@gmail.com>
2020-05-19 16:00:02 +0300

    Initial Commit

--- DatingApp.API/Data/AuthRepository.cs
+++ DatingApp.API/Data/AuthRepository.cs
@@ -62,9 +62,12 @@ namespace DatingApp.API.Data
             
         }
 
-        public Task<bool> UserExists(string username)
+        public async Task<bool> UserExists(string username)
         {
-            throw new System.NotImplementedException();
+            if(await _context.Users.AnyAsync(x => x.Username == username))
+                return true;
+
+            return false;
         }
     }
 }
